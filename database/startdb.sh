0ed385e2595f622412f2c0735918ac405a597018
drox13 <dario.baron@uptc.edu.co>
2021-09-09 12:48:57 -0500

    permisos de ejecucion

--- database/startdb.sh
+++ database/startdb.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-mysql -u root -pa -e "create database sanmplevideo_db; GRANT ALL PRIVILEGES ON sanmplevideo_db.* TO 'root'@'localhost' WITH GRANT OPTION"
-mysql -u root -pa sanmplevideo_db < Sample-SQL-File-10rows.sql
+service mysql restart
+mysql -u root -e "create database sanmplevideo_db; GRANT ALL PRIVILEGES ON sanmplevideo_db.* TO 'root'@'localhost' WITH GRANT OPTION"
+mysql -u root sanmplevideo_db < Sample-SQL-File-10rows.sql
