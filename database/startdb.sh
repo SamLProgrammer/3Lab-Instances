8f62e3afb5f4ccf26fd1e61d88cbd5271b77a941
SamLProgrammer <86812263+SamLProgrammer@users.noreply.github.com>
2021-09-09 15:22:38 -0500

    corregir otra vez

--- database/startdb.sh
+++ database/startdb.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 service mysql restart
-mysql -u root -e "create database sanmplevideo_db; GRANT ALL PRIVILEGES ON samplevideo_db.* TO 'root'@'localhost' WITH GRANT OPTION"
+mysql -u root -e "create database samplevideo_db; GRANT ALL PRIVILEGES ON samplevideo_db.* TO 'root'@'localhost' WITH GRANT OPTION"
 mysql -u root samplevideo_db < Sample-SQL-File-10rows.sql
