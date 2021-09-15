3cc4241c7186949592486c7436aafbff9e0822b6
Samuel López <samuel.lopez@uptc.edu.co>
2021-09-15 00:04:42 -0500

    2

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const express = require('express')
 const app = express()
 const shell = require('shelljs')
 let mysql = require('mysql');
-const { exec } = require('child_process');
-
+const { spawn } = require('child_process');
 
 const port = 3000
 const PATH = process.cwd();
 
+const ls = spawn("ls", ["-la"]);
+
 app.get('/cpuStatus', (req, res) => {
-  exec('sh bashes/cpuMonitor.sh',
-  (error, stdout, stderr) => {
-      console.log(stdout);
-      res.send({msg: stdout});
-      console.log(stderr);
-      if (error !== null) {
-          console.log(`exec error: ${error}`);
-      }
+  ls.stdout.on("data", data => {
+    console.log(`stdout: ${data}`);
+    res.send({msg: data})
   });
 })
+
+
+// app.get('/cpuStatus', (req, res) => {
+//   exec('sh bashes/cpuMonitor.sh',
+//   (error, stdout, stderr) => {
+//       console.log(stdout);
+//       res.send({msg: stdout});
+//       console.log(stderr);
+//       if (error !== null) {
+//           console.log(`exec error: ${error}`);
+//       }
+//   });
+// })
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -36,7 +45,7 @@ app.listen(port, () => {
 
 app.get('/query', (req, res) => {
   res.send({ id: req.query.id })
-  con.connect(function(err) {
+  con.connect(function (err) {
     if (err) throw err;
     con.query("SELECT name FROM users where user_id = " + req.query.id, function (err, result, fields) {
       if (err) throw err;
